7a02d799a5b7d4cdf86ca73d7ed9800638d18899
syed muhammad taha <tahapk58752@gmail.com>
2022-08-28 00:54:35 +0500

    taha

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomeScreen from "./Screens/MainScreen/HomeScreen";
 function App() {
 // HEAD
   console.log('Hello world ');
-  console.log('taha request');
+  console.log('taha request completed');
 
   return (
     <div className="App border-red-500 border-1  w-[100%] flex flex-col">
