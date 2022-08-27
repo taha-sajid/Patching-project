8dc674141752e1cba1c6908957c3f09a78f31544
Mudassir164 <mudassirahmed16164@gmail.com>
2022-08-27 11:03:06 -0700

    Header Complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomeScreen from "./Screens/MainScreen/HomeScreen";
 
 
 function App() {
+  console.log('Taha sajid is a baap of all programmers')
   return (
     <div className="App border-red-500 border-1  w-[100%] flex flex-col">
      <TopNavbar/>
