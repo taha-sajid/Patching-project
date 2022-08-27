7707d0a29bfd4ae069f6557bba37df1f82d6cd19
Mudassir164 <mudassirahmed16164@gmail.com>
2022-08-27 11:47:01 -0700

    finalizing multi tasking project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import HomeScreen from "./Screens/MainScreen/HomeScreen";
 
 
 function App() {
-  console.log('Taha sajid bklol he');
+  console.log('Taha sajid bklol and ghanchu he');
   return (
     <div className="App border-red-500 border-1  w-[100%] flex flex-col">
      <TopNavbar/>
